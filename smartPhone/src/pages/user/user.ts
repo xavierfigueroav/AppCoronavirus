5ce501b9802cea90b1030d6182abbbcaa494dbaa
Xavier Figueroa <xavierfigueroav@gmail.com>
2020-04-04 00:14:34 -0500

    Add quarantine and risk animations to user tab.
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

--- smartPhone/src/pages/user/user.ts
+++ smartPhone/src/pages/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { AuthPage } from '../auth/auth';
@@ -8,14 +8,39 @@ import { AuthPage } from '../auth/auth';
 	templateUrl: 'user.html',
 })
 
-export class UserPage {
+export class UserPage implements OnInit{
+
+    counter: number;
+    color0: string;
+    color1: string;
+    color2: string;
+    color3: string;
 
 	constructor(public navCtrl: NavController, public navParams: NavParams, public appCtrl: App, private storage: Storage) {
-	}
+    }
 
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad UserPage');
-	}
+    ngOnInit() {
+        this.counter = 0.0;
+        console.log('ionViewDidLoad UserPage');
+        setInterval(() => {
+            this.color0 = this.changeColor(this.counter);
+            this.color1 = this.changeColor((this.counter + 0.3) % 1);
+            this.color2 = this.changeColor((this.counter + 0.6) % 1);
+            this.color3 = this.changeColor((this.counter + 0.9) % 1);
+            this.counter = (this.counter + 0.01) % 1;
+        }, 125);
+
+    }
+
+    changeColor(score: number) {
+        if(score <= 0.5) {
+            const red = 55 + Math.round(score*400);
+            return `rgb(${red},200,0)`;
+        } else {
+            const green = 200 - Math.round((score - 0.5)*400);
+            return `rgb(255,${green},0)`;
+        }
+    }
 
 	cerrarSesion() {
         this.storage.get('linkedUser').then((val) => {
@@ -24,5 +49,4 @@ export class UserPage {
             });
         });
     }
-
 }
