2cc1398ecd591939c08df3af384624b3ceb30fb9
Xavier Figueroa <xavierfigueroav@gmail.com>
2020-04-05 03:43:07 -0500

    Replace concentric circles with color bar
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

--- smartPhone/src/pages/user/user.ts
+++ smartPhone/src/pages/user/user.ts
@@ -11,28 +11,52 @@ import { AuthPage } from '../auth/auth';
 export class UserPage implements OnInit{
 
     counter: number;
-    color0: string;
-    color1: string;
-    color2: string;
-    color3: string;
+    ringColor: string;
+    scores: any;
 
 	constructor(public navCtrl: NavController, public navParams: NavParams, public appCtrl: App, private storage: Storage) {
     }
 
     ngOnInit() {
+        console.log('ngOnInit UserPage');
+        this.scores = [
+            {hour: 1, score: 0.5 },
+            {hour: 2, score: 0.3 },
+            {hour: 3, score: 0.2 },
+            {hour: 4, score: 0.7 },
+            {hour: 5, score: 0.9 },
+            {hour: 6, score: 0.3 },
+            {hour: 7, score: 0.2 },
+            {hour: 8, score: 0.0 },
+            {hour: 9, score: 0.4 },
+            {hour: 10, score: 0.6 },
+            {hour: 11, score: 0.8 },
+            {hour: 12, score: 0.1 },
+            {hour: 13, score: 0.0 },
+            {hour: 14, score: 0.8 },
+            {hour: 15, score: 0.9 },
+            {hour: 16, score: 0.3 },
+            {hour: 17, score: 0.2 },
+            {hour: 18, score: 0.7 },
+            {hour: 19, score: 0.1 },
+            {hour: 20, score: 0.2 },
+            {hour: 21, score: 0.1 },
+            {hour: 22, score: 0.6 },
+            {hour: 23, score: 0.9 },
+            {hour: 24, score: 0.5 }
+        ];
+        this.scores.forEach((score: any) => {
+            score.color = this.getColorByScore((score.score));
+        });
         this.counter = 0.0;
-        console.log('ionViewDidLoad UserPage');
         setInterval(() => {
-            this.color0 = this.changeColor(this.counter);
-            this.color1 = this.changeColor((this.counter + 0.3) % 1);
-            this.color2 = this.changeColor((this.counter + 0.6) % 1);
-            this.color3 = this.changeColor((this.counter + 0.9) % 1);
+            this.ringColor = this.getColorByScore(this.counter);
             this.counter = (this.counter + 0.01) % 1;
         }, 125);
 
     }
 
-    changeColor(score: number) {
+    getColorByScore(score: number) {
         if(score <= 0.5) {
             const red = 55 + Math.round(score*400);
             return `rgb(${red},200,0)`;
