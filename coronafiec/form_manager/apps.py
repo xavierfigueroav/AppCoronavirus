84449c3d91c26132e3adffde52027b9f9bd6f2eb
Xavier Figueroa <xavierfigueroav@gmail.com>
2020-05-26 01:46:22 -0500

    Delete old web project and rename mobile app directory
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

--- coronafiec/form_manager/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class FormManagerConfig(AppConfig):
-    name = 'form_manager'
--- coronafiec/static/js/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $('.dtBasicExample').each(function (index){
-    $(this).DataTable();
-    $('.dataTables_length').addClass('bs-select');
-  })
-});
